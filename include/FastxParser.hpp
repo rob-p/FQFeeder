521ac6246792f43da7ce326830a073bf698dec33
Rob Patro <rob.patro@cs.stonybrook.edu>
2016-06-04 13:40:01 -0400

    cleanup interface

--- include/FastxParser.hpp
+++ include/FastxParser.hpp
@@ -30,33 +30,33 @@ extern "C" {
 #include <utility>
 
     template<class T> struct _Unique_if {
-        typedef unique_ptr<T> _Single_object;
+        using _Single_object = std::unique_ptr<T>;
     };
 
     template<class T> struct _Unique_if<T[]> {
-        typedef unique_ptr<T[]> _Unknown_bound;
+        using _Unknown_bound = std::unique_ptr<T[]>;
     };
 
     template<class T, size_t N> struct _Unique_if<T[N]> {
-        typedef void _Known_bound;
+        using _Known_bound = void;
     };
 
     template<class T, class... Args>
         typename _Unique_if<T>::_Single_object
         make_unique(Args&&... args) {
-            return unique_ptr<T>(new T(std::forward<Args>(args)...));
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
         }
 
     template<class T>
         typename _Unique_if<T>::_Unknown_bound
         make_unique(size_t n) {
-            typedef typename remove_extent<T>::type U;
-            return unique_ptr<T>(new U[n]());
+        using U = typename std::remove_extent<T>::type;
+            return std::unique_ptr<T>(new U[n]());
         }
 
     template<class T, class... Args>
         typename _Unique_if<T>::_Known_bound
-        make_unique(Args&&...) = delete;
+    make_unique(Args&&...) = delete;
 
 #endif // C++11
 #endif //__FASTX_PARSER_PRECXX14_MAKE_UNIQUE__
